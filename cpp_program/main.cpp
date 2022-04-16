c5c41fe00484522d18218ce1fe67fb5cf2299916
H3cth0r <hectorsucre13@gmail.com>
2022-04-16 00:51:42 -0500

    application of class reader

--- cpp_program/main.cpp
+++ cpp_program/main.cpp
@@ -3,6 +3,12 @@
 
 int main(){
 
+   std::string file_path = "../txt_tabs/back_in_black.txt";
+   Reader the_reader = Reader(file_path);
+   //the_reader.print_vector();
+   the_reader.translate_tabs();
+   std::cout << the_reader.get_output_txt() << "\n";
+   the_reader.export_output_to_txt("../output_txt/back_in_black.txt");
 
 
     return 0;
