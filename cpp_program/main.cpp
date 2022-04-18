ea89d03c49ec66fddd7a24ad963887fff2b0790f
H3cth0r <hectorsucre13@gmail.com>
2022-04-18 08:10:57 -0500

    new output

--- cpp_program/main.cpp
+++ cpp_program/main.cpp
@@ -3,12 +3,12 @@
 
 int main(){
 
-   std::string file_path = "../txt_tabs/back_in_black.txt";
+   std::string file_path = "../txt_tabs/boys_dont_cry.txt";
    Reader the_reader = Reader(file_path);
    //the_reader.print_vector();
    the_reader.translate_tabs();
    std::cout << the_reader.get_output_txt() << "\n";
-   the_reader.export_output_to_txt("../output_txt/back_in_black.txt");
+   the_reader.export_output_to_txt("../output_txt/boys_dont_cry.txt");
 
 
     return 0;
