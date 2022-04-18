169c2d491c13dcf20c7f69aeb8af8ae6e38ad6a2
H3cth0r <hectorsucre13@gmail.com>
2022-04-18 08:46:28 -0500

    fixed format

--- cpp_program/main.cpp
+++ cpp_program/main.cpp
@@ -3,12 +3,12 @@
 
 int main(){
 
-   std::string file_path = "../txt_tabs/paranoid.txt";
+   std::string file_path = "../txt_tabs/seven_nation_army.txt";
    Reader the_reader = Reader(file_path);
    //the_reader.print_vector();
    the_reader.translate_tabs();
    std::cout << the_reader.get_output_txt() << "\n";
-   the_reader.export_output_to_txt("../output_txt/paranoid.txt");
+   the_reader.export_output_to_txt("../output_txt/seven_nation_army.txt");
 
 
     return 0;
